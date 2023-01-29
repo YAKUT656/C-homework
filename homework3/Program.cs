fb7f5f8de86a244fed1a9898ca805dadb93fe1d1
Sanzhar <sanzhar656@gmail.com>
2023-01-29 11:13:14 +0600

    добавлен homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.Write("Введите число 1: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
