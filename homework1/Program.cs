17177ec45c0fe81cda96105c733857ef731cf27b
Sanzhar <sanzhar656@gmail.com>
2023-01-29 10:12:09 +0600

    добавил домашнеезадание№2

---
+++ homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Введите число 1: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number>number2)
+{
+    Console.WriteLine($"{number}");
+}
+else
+{
+    Console.WriteLine($"{number2}");
+}
