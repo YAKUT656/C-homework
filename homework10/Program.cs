e2034ba7f1ccddef478105f284403ebfd0325fd2
YAKUT656 <123108517+YAKUT656@users.noreply.github.com>
2023-02-02 17:04:36 +0600

    dobavlena zadacha #10

---
+++ homework10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное число : ");
+int number = int.Parse(Console.ReadLine()!);
+int number2 = number/10;
+int number3 = number2 % 10;
+Console.Write(number3);
