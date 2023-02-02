461d5e7fa0e8f2ce76bf94aeae0ab175d76b79e3
YAKUT656 <123108517+YAKUT656@users.noreply.github.com>
2023-02-02 17:05:08 +0600

    dobavlena zadacha #13

---
+++ homework#13/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трех значное число");
+int number = int.Parse(Console.ReadLine()!);
+
+while (number>999)
+{
+    number = number / 10;
+}
+int result = number % 10;
+
+if (number<99)
+{
+    Console.WriteLine($"Третьего числа нет"!);
+}
+else
+{
+    Console.WriteLine($"{result}"!);
+}
